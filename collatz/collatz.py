4cf86350f65da17d00c693ecb690791901402e87
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2022-06-13 16:42:25 -0700

    ...

---
+++ collatz/collatz.py
@@ -0,0 +1,18 @@
+f = open("C://Users//kyeou//Dropbox//PC (2)//Desktop//Stuff On Github//Kyeou-Python//collatz//results.txt", 'w')
+
+for i in range(1, 5000000):
+    number = i
+    f.write(str(number))
+    f.write(": ")
+    while (number != 1):
+        if (number % 2 == 0):
+            number = number/2
+            f.write(str(number))
+            f.write(" ")
+        else:
+            number = (number*3)+1
+            f.write(str(number))
+            f.write(" ")
+    f.write("\n")
+    print(i)
+f.close()
