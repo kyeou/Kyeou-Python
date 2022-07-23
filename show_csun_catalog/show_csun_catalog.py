2ee802dce9f05df139143c92492b1f23a1ddfa75
kyeou <CHRISTIANJARMON630@GMAIL.COM>
2022-07-22 23:40:42 -0700

    formatting

--- show_csun_catalog/show_csun_catalog.py
+++ show_csun_catalog/show_csun_catalog.py
@@ -1,3 +1,4 @@
+from selenium.common.exceptions import NoSuchElementException
 import sys
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -12,22 +13,26 @@ import time
 
 catalog_link = 'https://cmsweb.csun.edu/psc/CNRPRD/EMPLOYEE/SA/c/NR_SSS_COMMON_MENU.NR_SSS_SOC_BASIC_C.GBL?PortalActualURL=https%3a%2f%2fcmsweb.csun.edu%2fpsc%2fCNRPRD%2fEMPLOYEE%2fSA%2fc%2fNR_SSS_COMMON_MENU.NR_SSS_SOC_BASIC_C.GBL&PortalContentURL=https%3a%2f%2fcmsweb.csun.edu%2fpsc%2fCNRPRD%2fEMPLOYEE%2fSA%2fc%2fNR_SSS_COMMON_MENU.NR_SSS_SOC_BASIC_C.GBL&PortalContentProvider=SA&PortalCRefLabel=Class%20Search&PortalRegistryName=EMPLOYEE&PortalServletURI=https%3a%2f%2fmynorthridge.csun.edu%2fpsp%2fPANRPRD%2f&PortalURI=https%3a%2f%2fmynorthridge.csun.edu%2fpsc%2fPANRPRD%2f&PortalHostNode=EMPL&NoCrumbs=yes&PortalKeyStruct=yes'
 
+
 def gather_data():
     print("Semester: " + sys.argv[1])
     print("Year: " + sys.argv[2])
     print("Subject: " + sys.argv[3])
 
 
-section_number = [2233,2227,2225,2223,2221,2217,2215,2213,2211,2207,2205,2203,2201,2197,2195,2193,2191,2187,2185,2183,2181,2177,2175,2173,2171,2167,2165,2163,2161,2157,2155,2153,2151,2147,2145,2143,2141,2137,2135,2133,2131,2127,2125,2123,2121,2117,2115,2113,2111,2107,2105,2103,2101,2097,2095,2093,2091,2087,2085,2083,2081,2077,2075,2073,2071,2067,2065,2063,2061,2057,2055,2053,2051,2047,2045,2043,2041]
+section_number = [2233, 2227, 2225, 2223, 2221, 2217, 2215, 2213, 2211, 2207, 2205, 2203, 2201, 2197, 2195, 2193, 2191, 2187, 2185, 2183, 2181, 2177, 2175, 2173, 2171, 2167, 2165, 2163, 2161, 2157, 2155, 2153, 2151, 2147, 2145, 2143, 2141,
+                  2137, 2135, 2133, 2131, 2127, 2125, 2123, 2121, 2117, 2115, 2113, 2111, 2107, 2105, 2103, 2101, 2097, 2095, 2093, 2091, 2087, 2085, 2083, 2081, 2077, 2075, 2073, 2071, 2067, 2065, 2063, 2061, 2057, 2055, 2053, 2051, 2047, 2045, 2043, 2041]
+
+
 def match_st():
     term_string = sys.argv[1] + " " + sys.argv[2]
     match term_string:
         case 'Spring 2023':
             return section_number[0]
-        case 'Fall 2022':  
+        case 'Fall 2022':
             return section_number[1]
         case 'Summer 2022':
-            return section_number[2]  
+            return section_number[2]
         case 'Spring 2022':
             return section_number[3]
         case 'Winter 2022':
@@ -37,9 +42,9 @@ def match_st():
         case 'Summer 2021':
             return section_number[6]
         case 'Spring 2021':
-            return section_number[7]            
+            return section_number[7]
         case 'Winter 2021':
-            return section_number[8]            
+            return section_number[8]
         case 'Fall 2020':
             return section_number[9]
         case 'Summer 2020':
@@ -83,100 +88,101 @@ def match_st():
         case 'Fall 2015':
             return section_number[29]
         case 'Summer 2015':
-            return section_number[30]            
+            return section_number[30]
         case 'Spring 2015':
-            return section_number[31]            
+            return section_number[31]
         case 'Winter 2015':
-            return section_number[32]            
-        case 'Fall 2014':  
-            return section_number[33]          
+            return section_number[32]
+        case 'Fall 2014':
+            return section_number[33]
         case 'Summer 2014':
-            return section_number[34]            
+            return section_number[34]
         case 'Spring 2014':
-            return section_number[35]            
+            return section_number[35]
         case 'Winter 2014':
-            return section_number[36]            
-        case 'Fall 2013':  
-            return section_number[37]          
+            return section_number[36]
+        case 'Fall 2013':
+            return section_number[37]
         case 'Summer 2013':
-            return section_number[38]            
+            return section_number[38]
         case 'Spring 2013':
-            return section_number[39]            
+            return section_number[39]
         case 'Winter 2013':
-            return section_number[40]           
-        case 'Fall 2012':  
-            return section_number[41]          
+            return section_number[40]
+        case 'Fall 2012':
+            return section_number[41]
         case 'Summer 2012':
-            return section_number[42]           
+            return section_number[42]
         case 'Spring 2012':
-            return section_number[43]            
+            return section_number[43]
         case 'Winter 2012':
-            return section_number[44]            
-        case 'Fall 2011':  
-            return section_number[45]          
+            return section_number[44]
+        case 'Fall 2011':
+            return section_number[45]
         case 'Summer 2011':
-            return section_number[46]           
+            return section_number[46]
         case 'Spring 2011':
-            return section_number[47]           
+            return section_number[47]
         case 'Winter 2011':
-            return section_number[48]           
-        case 'Fall 2010':  
-            return section_number[49]        
+            return section_number[48]
+        case 'Fall 2010':
+            return section_number[49]
         case 'Summer 2010':
-            return section_number[50]        
+            return section_number[50]
         case 'Spring 2010':
-            return section_number[51]        
+            return section_number[51]
         case 'Winter 2010':
-            return section_number[52]        
-        case 'Fall 2009':  
-            return section_number[53]      
+            return section_number[52]
+        case 'Fall 2009':
+            return section_number[53]
         case 'Summer 2009':
-            return section_number[54]       
+            return section_number[54]
         case 'Spring 2009':
-            return section_number[55]        
+            return section_number[55]
         case 'Winter 2009':
-            return section_number[56]        
-        case 'Fall 2008':  
-            return section_number[57]      
+            return section_number[56]
+        case 'Fall 2008':
+            return section_number[57]
         case 'Summer 2008':
-            return section_number[58]       
+            return section_number[58]
         case 'Spring 2008':
-            return section_number[59]        
+            return section_number[59]
         case 'Winter 2008':
-            return section_number[60]      
-        case 'Fall 2007':  
-            return section_number[61]      
+            return section_number[60]
+        case 'Fall 2007':
+            return section_number[61]
         case 'Summer 2007':
-            return section_number[62]      
+            return section_number[62]
         case 'Spring 2007':
-            return section_number[63]      
+            return section_number[63]
         case 'Winter 2007':
-            return section_number[64]       
-        case 'Fall 2006':  
-            return section_number[65]    
+            return section_number[64]
+        case 'Fall 2006':
+            return section_number[65]
         case 'Summer 2006':
-            return section_number[66]      
+            return section_number[66]
         case 'Spring 2006':
-            return section_number[67]     
+            return section_number[67]
         case 'Winter 2006':
-            return section_number[68]     
-        case 'Fall 2005':  
-            return section_number[69]   
+            return section_number[68]
+        case 'Fall 2005':
+            return section_number[69]
         case 'Summer 2005':
-            return section_number[70]      
+            return section_number[70]
         case 'Spring 2005':
-            return section_number[71]     
+            return section_number[71]
         case 'Winter 2005':
-            return section_number[72]           
-        case 'Fall 2004':  
-            return section_number[73]       
+            return section_number[72]
+        case 'Fall 2004':
+            return section_number[73]
         case 'Summer 2004':
-            return section_number[74]        
+            return section_number[74]
         case 'Spring 2004':
-            return section_number[75]         
+            return section_number[75]
         case 'Winter 2004':
             return section_number[76]
-            
+
+
 gather_data()
 
 # Semester ID: NR_SSS_SOC_NWRK_STRM
@@ -186,7 +192,7 @@ s = Service(ChromeDriverManager().install())
 op = webdriver.ChromeOptions()
 op.add_argument('headless')
 op.add_experimental_option('excludeSwitches', ['enable-logging'])
-driver = webdriver.Chrome(service=s,options=op)
+driver = webdriver.Chrome(service=s, options=op)
 driver.get(catalog_link)
 time.sleep(4)
 
@@ -195,14 +201,14 @@ semester_box = driver.find_element("name", "NR_SSS_SOC_NWRK_STRM")
 semester_box.click()
 
 semester_box.send_keys(match_st())
-#time.sleep(1)
+# time.sleep(1)
 semester_box.send_keys(Keys.ENTER)
-#time.sleep(5)
+# time.sleep(5)
 
 
 id_box = driver.find_element("name", "NR_SSS_SOC_NWRK_SUBJECT")
 id_box.click()
-id_box.send_keys(sys.argv[3]) 
+id_box.send_keys(sys.argv[3])
 id_box.send_keys(Keys.ENTER)
 time.sleep(3)
 
@@ -210,7 +216,7 @@ driver.find_element("name", "NR_SSS_SOC_NWRK_BASIC_SEARCH_PB").click()
 time.sleep(2)
 
 # Class Section Div ID: win0divNR_SSS_SOC_NSEC$(INDEX)
-#   INDEX = classes listed in ascending order 
+#   INDEX = classes listed in ascending order
 
 # Sesn = NR_SSS_SOC_NSEC_SESSION_CODE$0
 # Section = NR_SSS_SOC_NSEC_CLASS_SECTION$0
@@ -222,35 +228,45 @@ time.sleep(2)
 # Days = NR_SSS_SOC_NWRK_DESCR20$0
 # Time = NR_SSS_SOC_NSEC_DESCR25_2$0
 # Instructor = FACURL$0
-from selenium.common.exceptions import NoSuchElementException 
 
 for a in range(0, 60):
     try:
-        print(driver.find_element("id","NR_SSS_SOC_NWRK_DESCR100_2$" + str(a)).text)
+        print(driver.find_element("id", "NR_SSS_SOC_NWRK_DESCR100_2$" + str(a)).text)
         driver.find_element("id", "win0divSOC_DETAIL$" + str(a)).click()
         time.sleep(1)
-        print("Session\tSection\tClass#\tSeats\tStatus\tComp\tLoc\tDays\tTime\t\t   Faculty")
+        print(
+            "Session\tSection\tClass#\tSeats\tStatus\tComp\tLoc\tDays\tTime\t\t   Faculty")
         for i in range(0, 20):
             try:
-                #print("Session\tSection\tClass#\tSeats\tStatus\tComp\tLoc\tDays\tTime\t\t   Faculty")
-                row_sesn = driver.find_element("id","NR_SSS_SOC_NSEC_SESSION_CODE$" + str(i)).text
-                row_section = driver.find_element("id","NR_SSS_SOC_NSEC_CLASS_SECTION$" + str(i)).text
-                row_class = driver.find_element("id","NR_SSS_SOC_NSEC_CLASS_NBR$" + str(i)).text
-                row_seats = driver.find_element("id","NR_SSS_SOC_NWRK_AVAILABLE_SEATS$" + str(i)).text
-                row_status = driver.find_element("id","NR_SSS_SOC_NWRK_DESCRSHORT$" + str(i)).text
-                row_comp = driver.find_element("id","NR_SSS_SOC_NSEC_SSR_COMPONENT$" + str(i)).text
-                row_loc = driver.find_element("id","MAP$" + str(i)).text
-                row_days = driver.find_element("id","NR_SSS_SOC_NWRK_DESCR20$" + str(i)).text
-                row_time = driver.find_element("id","NR_SSS_SOC_NSEC_DESCR25_2$" + str(i)).text
+                # print("Session\tSection\tClass#\tSeats\tStatus\tComp\tLoc\tDays\tTime\t\t   Faculty")
+                row_sesn = driver.find_element(
+                    "id", "NR_SSS_SOC_NSEC_SESSION_CODE$" + str(i)).text
+                row_section = driver.find_element(
+                    "id", "NR_SSS_SOC_NSEC_CLASS_SECTION$" + str(i)).text
+                row_class = driver.find_element(
+                    "id", "NR_SSS_SOC_NSEC_CLASS_NBR$" + str(i)).text
+                row_seats = driver.find_element(
+                    "id", "NR_SSS_SOC_NWRK_AVAILABLE_SEATS$" + str(i)).text
+                row_status = driver.find_element(
+                    "id", "NR_SSS_SOC_NWRK_DESCRSHORT$" + str(i)).text
+                row_comp = driver.find_element(
+                    "id", "NR_SSS_SOC_NSEC_SSR_COMPONENT$" + str(i)).text
+                row_loc = driver.find_element("id", "MAP$" + str(i)).text
+                row_days = driver.find_element(
+                    "id", "NR_SSS_SOC_NWRK_DESCR20$" + str(i)).text
+                row_time = driver.find_element(
+                    "id", "NR_SSS_SOC_NSEC_DESCR25_2$" + str(i)).text
                 try:
-                    row_faculty = driver.find_element("id","FACURL$" + str(i)).text       
-                except NoSuchElementException: 
-                    row_faculty = "Staff"     
-                print(row_sesn + "\t" + row_section + "\t" + row_class + "\t" + row_seats + "\t" + row_status + "\t" + row_comp + "\t" + row_loc + "\t" + row_days + "\t" + row_time + "\t   " +  row_faculty + "\n")
+                    row_faculty = driver.find_element(
+                        "id", "FACURL$" + str(i)).text
+                except NoSuchElementException:
+                    row_faculty = "Staff"
+                print(row_sesn + "\t" + row_section + "\t" + row_class + "\t" + row_seats + "\t" + row_status +
+                      "\t" + row_comp + "\t" + row_loc + "\t" + row_days + "\t" + row_time + "\t   " + row_faculty + "\n")
             except NoSuchElementException:
                 i = 20
         driver.find_element("id", "win0divSOC_DETAIL1$" + str(a)).click()
-        time.sleep(1)        
+        time.sleep(1)
     except NoSuchElementException:
         a = 60
 
