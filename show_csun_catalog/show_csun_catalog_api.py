bc533c05c18bd903af82cbff8cb8de2b7adcccb2
kyeou <christianjarmon630@gmail.com>
2022-07-23 17:16:11 -0700

    adding some documentation comments

--- show_csun_catalog/show_csun_catalog_api.py
+++ show_csun_catalog/show_csun_catalog_api.py
@@ -6,6 +6,8 @@ url = u"https://api.metalab.csun.edu/curriculum/api/2.0/terms/" + sys.argv[1] + 
                                                                   sys.argv[2] + "/classes/" + \
                                                                   sys.argv[3].lower()
 
+print("Data Link:" + url)
+
 #try to read the data
 try:
     u = urllib3.PoolManager().request("GET", url)
@@ -16,28 +18,37 @@ except Exception as e:
 #decode into an array
 data = json.loads(data)
 
-#setup a blank array
+"""
+setup a blank array for all the strings to 
+mass print them after all processing is done
+"""
 blob_list = []
 
-#loop through results and add each course's subject
-#and catalog number to blob_list array (i.e COMP 100)
 current_class = ""
 for course in data['classes']:
-    if (current_class != course["title"]):
+    
+    
+    if (current_class != course["title"]): # print out sections every class itself (110, 122, 182 etc...)
+        # every new name (COMP 100 vs COMP 110) will start new blob
         blob_list.append("\n\n--------------\n")
         current_class = course["title"]
         blob_list.append(course['subject'] + ' ' + course['catalog_number'] + ' ' + course["title"])
         blob_list.append("")
         blob_list.append("\tSection\t\tLocation\tDays\t\tSeats Aval\t\tTime\t\t\t\tFaculty")
         blob_list.append("\t-------\t\t--------\t----\t\t----------\t\t----\t\t\t\t-------")
-    if (len(course["meetings"]) > 0):
+        
+        
+    if (len(course["meetings"]) > 0): # if a class has no meetings, it should not be on schedule
         section_string = []
         #section_string.append(course['subject'] + ' ' + course['catalog_number'])
         section_string.append("\t" + course["class_number"])
-        if (len(course["meetings"][0]["location"]) != 7):
+        
+        if (len(course["meetings"][0]["location"]) != 7): 
+            # (JD1600A is one character longer than all other class location strings, so it messes up tabs)
             section_string.append("\t\t" + course["meetings"][0]["location"])
         else:
             section_string.append("\t       " + course["meetings"][0]["location"])
+            
         section_string.append("\t\t" + course["meetings"][0]["days"])
         section_string.append("\t\t" + str(course["enrollment_cap"] - course["enrollment_count"]))
         section_string.append("\t\t    " +
@@ -47,7 +58,8 @@ for course in data['classes']:
                               " - " +
                               (course["meetings"][0]["end_time"])[0:2] + ":" +
                               (course["meetings"][0]["end_time"])[2:4])
-        if (len(course["instructors"]) > 0):
+        
+        if (len(course["instructors"]) > 0): # if a class has no instructor, print Staff instead
             section_string.append("\t\t" + course["instructors"][0]["instructor"])
         else:
             section_string.append("\t\t\t" + "Staff")
